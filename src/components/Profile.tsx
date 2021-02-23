a9e22c1856960ea31e8cdef0d02414aa012a5081
Valirio <junior.braatz@hotmail.com>
2021-02-23 20:07:34 -0300

    meio da segunda aula

---
+++ src/components/Profile.tsx
@@ -0,0 +1,11 @@
+export function Profile(){
+    return(
+       <div>
+           <img src="https://github.com/Valirio.png" alt="Logo"/>
+           <div>
+               <strong>Nome</strong>
+               <p>Level</p>
+           </div>
+       </div> 
+    );
+}
